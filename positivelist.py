cec0466bb07e0b15f551cee8144a61eddbf60772
Shruti Dey <90207066+shrutidey99@users.noreply.github.com>
2021-09-30 00:33:54 +0530

    Add files via upload

---
+++ positivelist.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+list1 = [12,-7,5,64,-14]
+  
+for num in list1:
+    if num >= 0:
+       print(num, end = " ")
+       
+list2 = [12,14,-95,3]
+for num in list2:
+    if num >= 0:
+       print("\n", num,end = " ")      
